16efb9cf0ab01407db21143d0d484ab519ebeb76
chad svastisalee <swasgrafx@gmail.com>
2021-09-15 09:14:23 -0500

    XX Lab 1 - Use a Layout

--- public/thanks.php
+++ public/thanks.php
@@ -1,48 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Thank you for contacting MicroTrain2108 Chicago West Loop">
-    <meta name="keywords" content="thank you, thanks, MicroTrain2108, webdesign, development, web developer, Chicago West Loop">
-    <title>MicroTrain2108 Thanks</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
-    <meta name="msapplication-config" content="ieconfig.xml" />
-    <link rel="manifest" href="site.webmanifest">
-    <link rel="stylesheet" type="text/css" href="./dist/css/main.min.css">
-    </head>
-    <header>
-      <span class="logo">MicroTrain2108</span>
-      <a id="toggleMenu">Menu<a>
-        <nav>
-        <ul>
-          <li><a href="index.html">Home</a></li>
-          <li><a href="resume.html">Resume</a></li>
-          <li><a href="contact.html">Contact</a></li>
-        </ul>
-      </nav>
-    </header>    
-    <body>
-      <main>
-<h1>Thank you for contacting MicroTrain2108</h1>
-<p>Thanks for messaging me for webdesign, development web developmet or project in Chicago West Loop.</p>
-</main>    
-<script>
+<?php
+require '../core/bootstrap.php';
 
-  var toggleMenu = document.getElementById('toggleMenu');
-  var nav = document.querySelector('nav');
-  toggleMenu.addEventListener(
-    'click',
-    function(){
-      if(nav.style.display=='block'){
-        nav.style.display='none';
-      }else{
-        nav.style.display='block';
-      }
-    }
-  );
-</script>
-</body>
-</html>
+//Build the page metadata
+$meta = [];
+$meta['description'] = "Thank you MicroTrain2108 For Web Development";
+$meta['keywords'] = "Thanks, MicroTrain2108, Web development, web development";
+
+
+$content = <<<EOT
+  <main>
+    Thank you for contacting MicroTrain2108
+  </main>
+  EOT;
+
+  include '../core/layout.php';
